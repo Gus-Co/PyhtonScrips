f4aaff6c8e4342603dbbd076a23942fc8c03c793
GusTank <42395204+Gus-Co@users.noreply.github.com>
2023-09-29 11:07:39 +0200

    Update findphone.py

--- findphone.py
+++ findphone.py
@@ -3,6 +3,6 @@ from phonenumbers import geocoder
 # install the phonenumbers library
 # pip phonenumbers
 
-first_num = phonenumbers.parse("+27670835563") #input your Phone number inclued your country code
+first_num = phonenumbers.parse("+") #input your Phone number inclued your country code
 
 print(geocoder.description_for_number(first_num, "en"))
